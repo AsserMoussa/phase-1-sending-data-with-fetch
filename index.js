50e50508a7a3f94861e815459585eabbcdb209ea
assermoussa <asser.y.moussa@gmail.com>
2024-02-09 17:36:51 -0600

    Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Add your code here
+function submitData(name , email){
+
+   
+    return fetch("http://localhost:3000/users", {
+     
+    // Adding method type
+    method: "POST",
+     
+    body: JSON.stringify({
+        name: name,
+        email: email
+    }),
+     
+    // Adding headers to the request
+    headers: {
+        "Content-Type": "application/json", 
+        "Accept" : "application/json"
+    }
+})
+ 
+// Converting to JSON
+.then(response => response.json())
+ 
+// Displaying results to console
+.then(json => {
+    document.body.innerHTML = json.id})
+
+   
+
+   .catch (error =>{
+
+    console.error("There has been a problem with your fetch operation:", error);
+        document.body.innerHTML = error; 
+   })
+  }
